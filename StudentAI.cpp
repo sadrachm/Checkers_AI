36af0784be1db50c40b499060fd2fe1728747f45
Sadrach Martinez <63802470+sadrachm@users.noreply.github.com>
2021-11-17 15:51:26 -0800

    updates

--- StudentAI.cpp
+++ StudentAI.cpp
@@ -22,7 +22,8 @@ Move StudentAI::GetMove(Move move)
     }
     turn += 1;
     vector<vector<Move> > moves = board.getAllPossibleMoves(player);
-    Move bestMove = moves[0][0];
+    Move bestMove = moves[1][0];
+    MCTS(bestMove, player ==2 ? true: false);
     int maxHeuristic = -100;
     int minHeuristic = 100;
     int temp;
@@ -99,3 +100,41 @@ int StudentAI::MiniMax (Move move, bool mainPlayer) {
     return 0;
 }
 
+int StudentAI::MCTS (Move move, bool mainPlayer) {
+    int count = 0;
+    int value = 0;
+    int total = 0;
+    board.makeMove(move, mainPlayer ? 2 : 1);
+    board.showBoard();
+    while (count < 1) {
+        int numOfMoves = 0;
+        while (!board.isWin(player)) {
+            vector<vector<Move>> x = board.getAllPossibleMoves(mainPlayer ? 1 : 2);
+            int i = rand() % (x.size());
+            vector<Move> checker_moves = x[i];
+            int j = rand() % (checker_moves.size());
+            Move res = checker_moves[j];
+            board.makeMove(res,mainPlayer ? 1 : 2);
+            board.showBoard();
+            mainPlayer = !mainPlayer;
+            numOfMoves++;
+        }
+        for (int a = 0; a < numOfMoves; a++) {
+            board.Undo();
+            board.showBoard();
+        }
+        int x = board.isWin(player);
+        if (x == 1) {
+            value += 1;
+            total += 1;
+        } else {
+            total += 1;
+        }
+
+        count++;
+    }
+    board.Undo();
+    board.showBoard();
+    throw;
+    return 0;
+}
